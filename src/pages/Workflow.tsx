a1b85be68efcbf2da1083d19b7eec6e4a6bf95df
Armin Amirfatahi <arminfatahi2002@gmail.com>
2025-03-22 14:52:38 +0330

    some workflow cleanups and ReactFlow implementation

--- src/pages/Workflow.tsx
+++ src/pages/Workflow.tsx
@@ -1,98 +1,108 @@
-import { useLayoutEffect, useState } from "react";
+import { useCallback } from "react";
+
+import NodeBox from "../components/NodeBox";
+
+import ReactFlow, {
+  Edge,
+  Connection,
+  addEdge,
+  Background,
+  Controls,
+  MiniMap,
+  useEdgesState,
+  useNodesState,
+} from "reactflow";
+
+import "reactflow/dist/style.css";
+
+import SidePanel from "../components/SidePanel";
+
+const initialNodes = [
+  {
+    id: "1",
+    type: "customNode",
+    position: { x: 0, y: 0 },
+    selected: false,
+    data: { label: "Hi", type: "input" },
+  },
+  {
+    id: "2",
+    type: "customNode",
+    position: { x: 150, y: 150 },
+    selected: false,
+    data: { label: "Middle", type: "input-output" },
+  },
+  {
+    id: "3",
+    type: "customNode",
+    position: { x: 300, y: 300 },
+    selected: false,
+    data: { label: "Bye", type: "output" },
+  },
+];
+
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+const nodeTypes = { customNode: NodeBox };
 
 function Workflow() {
-  const panelStates: readonly string[] = ["w-12 sm:w-40", "w-12", "w-40"];
-  const arrowStates: readonly string[] = [
-    "scale-x-[-1] sm:scale-x-[1]",
-    "scale-x-[-1]",
-    "",
-  ];
-  const [panelState, setPanelState] = useState(0);
+  // Edges and Nodes
+  const [nodes, setNodes, onNodeChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  function handleExpand(): void {
-    if (panelState === 0) {
-      if (window.innerWidth < 800) {
-        setPanelState(2);
-      } else {
-        setPanelState(1);
-      }
-    } else if (panelState === 1) {
-      if (window.innerWidth < 800) {
-        setPanelState(2);
-      } else {
-        setPanelState(0);
-      }
-    } else {
-      setPanelState(1);
-    }
-    console.log(window.innerWidth);
-  }
-
-  // Must Be Added to Hooks Folder?
-  useLayoutEffect(() => {
-    function updateExpand() {
-      if (panelState === 2 && window.innerWidth < 800) {
-        setPanelState(0);
-      }
-    }
-    window.addEventListener("resize", updateExpand);
-
-    return () => window.removeEventListener("resize", updateExpand);
-  }, [panelState]);
-  //
+  const onConnect = useCallback(
+    (connection: Edge | Connection) =>
+      setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
 
   return (
     <div className="h-screen text-white">
       <div className="flex flex-row h-full divide-x divide-primary">
-        <div
-          className={`bg-secondary shrink-0 transition-all ease-in-out duration-300 ${panelStates[panelState]}`}
-        >
-          <div className="flex flex-col gap-20 items-center">
-            <div className="flex flex-col gap-10">
-              <div className="bg-red-400 w-5 h-5"></div>
-              <div className="bg-red-400 w-5 h-5"></div>
-              <div className="bg-red-400 w-5 h-5"></div>
-              <div className="bg-red-400 w-5 h-5"></div>
-            </div>
-            <div className="w-full h-full ml-4 z-1">
-              <button
-                onClick={handleExpand}
-                className="cursor-pointer w-4 h-4 bg-gray-600 rounded-full float-right border-1 border-gray-400"
-              >
-                <div className="text-white hover:text-red-500 w-full h-full content-center">
-                  <svg
-                    className={`w-3 h-3 float-right align-middle ${arrowStates[panelState]}`}
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 16"
-                  >
-                    <path
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"
-                    />
-                  </svg>
-                </div>
-              </button>
-            </div>
-            <div className="flex flex-col gap-10">
-              <div className="bg-red-400 w-5 h-5"></div>
-              <div className="bg-red-400 w-5 h-5"></div>
-              <div className="bg-red-400 w-5 h-5"></div>
-              <div className="bg-red-400 w-5 h-5"></div>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-col w-full divide-y divide-primary">
+        <SidePanel />
+        <div className="flex flex-col w-full h-full divide-y divide-primary">
           <div className="h-15 bg-secondary shrink-0 px-5">
             <div className="">top right</div>
           </div>
 
-          <div className="bg-overlay-light h-full px-5">bottom right</div>
+          <div className="bg-overlay-light w-full h-full">
+            <div className="w-full h-full bg-gray-700 relative">
+              <div className="flex absolute z-1 right-0 items-center justify-center cursor-pointer w-12 h-10 border-2 rounded-xl mr-5 mt-5 hover:border-red-400 group">
+                <svg
+                  className="w-6 h-6 text-gray-800 dark:text-white group-hover:text-red-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 12h14m-7 7V5"
+                  />
+                </svg>
+              </div>
+              <div className="w-full h-full border border-gray-700 rounded-lg">
+                <ReactFlow
+                  nodes={nodes}
+                  edges={edges}
+                  onNodesChange={onNodeChange}
+                  onEdgesChange={onEdgesChange}
+                  onConnect={onConnect}
+                  fitView
+                  nodeTypes={nodeTypes}
+                >
+                  <Background />
+                  <MiniMap pannable={true} zoomable={true} />
+                  <Controls />
+                </ReactFlow>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
