608598f1360f3519e4b3821677b059dd2f196604
Armin Amirfatahi <101344906+arminfatahi@users.noreply.github.com>
2025-03-24 16:00:58 +0330

    adding workflow to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Login from './pages/login'
-import Signup from './pages/signup';
-import Verification from './pages/verification';
-import ForgetPassword from './pages/forgetPassword';
-import LoginVerification from './pages/loginVerification';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Verification from "./pages/verification";
+import ForgetPassword from "./pages/forgetPassword";
+import LoginVerification from "./pages/loginVerification";
+import Workflow from "./pages/Workflow";
 
 function App() {
-
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path='/verification' element={<Verification />} />
-          <Route path='/forgetPassword' element={<ForgetPassword />} />
-          <Route path='/loginVerification' element={<LoginVerification />} />
+          <Route path="/verification" element={<Verification />} />
+          <Route path="/forgetPassword" element={<ForgetPassword />} />
+          <Route path="/loginVerification" element={<LoginVerification />} />
+          <Route path="/workflow" element={<Workflow />} />
         </Routes>
       </Router>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
