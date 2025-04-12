09b6e68a951396d3ae4467f7abeb46a88428cae9
Armin Amirfatahi <arminfatahi2002@gmail.com>
2025-04-12 20:22:33 +0330

    adding deleting component feature coonection to back, changing param url

--- src/pages/Workflow.tsx
+++ src/pages/Workflow.tsx
@@ -1,22 +1,22 @@
 import { useParams } from "react-router-dom";
 import DnDFlow from "../components/DndFlow";
 
-type WorkflowParams = {
-  id: string;
+export type WorkflowParams = {
+  botId: string;
 };
 
 function Workflow() {
-  const { id } = useParams<WorkflowParams>();
+  const { botId } = useParams<WorkflowParams>();
   return (
     <>
-      {!id || isNaN(Number(id)) ?
+      {!botId || isNaN(Number(botId)) ?
         <div>Not a Number</div>
       : <div className="h-screen overflow-hidden text-white">
           <div className="flex h-full w-full flex-col divide-y divide-white text-gray-800">
             <div className="h-15 shrink-0 bg-patina-300 px-5">
               <div className="">title whatever</div>
             </div>
-            <DnDFlow botId={Number(id)} />
+            <DnDFlow botId={Number(botId)} />
           </div>
         </div>
       }
