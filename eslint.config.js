bc26fccd8c476bee67328801ee72076f32bda22c
Ali Kamali <seyedalikamali1381@gmail.com>
2025-04-25 02:57:06 +0330

    fix: rediret to login page on 401

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,7 @@ export default tseslint.config(
     },
     rules: {
       "no-unused-vars": "off",
+      "@typescript-eslint/prefer-promise-reject-errors": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-misused-promises": [
         "error",
