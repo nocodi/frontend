dad25ee47788df0eb740ec3a0f89898b6240ca9e
Ali Kamali <seyedalikamali1381@gmail.com>
2025-04-29 16:29:38 +0330

    feat: adds back to home button

--- src/pages/Workflow.tsx
+++ src/pages/Workflow.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import DnDFlow from "../components/DndFlow";
 import { createContext, useContext, useState } from "react";
 import CodeGeneration from "../components/CodeGeneration";
@@ -27,13 +27,19 @@ function Workflow() {
       : <div className="h-screen overflow-hidden">
           <div className="flex h-full w-full flex-col divide-y divide-white text-gray-800">
             <div className="h-15 shrink-0 bg-base-200 px-5">
-              <div className="items-right flex h-full flex-row-reverse gap-3 text-primary">
-                <div className="my-auto">
-                  <CodeGeneration botId={Number(botId)} />
+              <div className="flex h-full items-center justify-between gap-3 text-primary">
+                <Link to="/dashboard" className="btn btn-outline">
+                  Back to Home
+                </Link>
+                <div className="flex items-center gap-3">
+                  <div className="my-auto">
+                    <CodeGeneration botId={Number(botId)} />
+                  </div>
+                  {loading ?
+                    <Loading size={30} />
+                     : <Check size={30} className="my-auto" />
+                  }
                 </div>
-                {loading ?
-                  <Loading size={30} />
-                : <Check size={30} className="my-auto" />}
               </div>
             </div>
             <loadingContext.Provider value={setLoading}>
