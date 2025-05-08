85d37975f4dd81d563f471a3c6bd020aa0e6fb21
AliAlizadeh11 <alializadeh.dev11@gmail.com>
2025-05-08 15:07:17 +0330

    feat: add toast error to query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,23 @@
 import "./index.css";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 import App from "./App.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { toast } from "react-toastify";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      toast(error.message);
+    },
+  }),
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
