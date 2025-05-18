f3b1f71388ca5758e255137842013c59a6223db3
Armin Amirfatahi <arminfatahi2002@gmail.com>
2025-05-19 02:53:04 +0330

    adding group node (temp)

---
+++ src/components/GroupNode.tsx
@@ -0,0 +1,25 @@
+import { Handle, NodeProps, Position } from "reactflow";
+import { ComponentType } from "../types/Component";
+
+export const GroupNode = ({
+  id,
+  data,
+  isConnectable,
+}: NodeProps<ComponentType>) => {
+  return (
+    <div className="group-node h-20 w-30">
+      <Handle
+        type="target"
+        position={Position.Left}
+        isConnectable={isConnectable}
+        isConnectableStart={false}
+        style={{
+          width: 1,
+          height: 8,
+          borderRadius: 1,
+        }}
+      />
+      <div className="">{data.component_name}</div>
+    </div>
+  );
+};
