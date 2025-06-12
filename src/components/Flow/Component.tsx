65c9d0ccbc34191823f326e8891766f089235d27
arminfatahi <arminfatahi2002@gmail.com>
2025-06-12 20:40:20 +0330

    remove GroupNode

--- src/components/Flow/Component.tsx
+++ src/components/Flow/Component.tsx
@@ -4,11 +4,16 @@ import { ComponentHeader, ComponentHandles } from "./ComponentOptions";
 import { sliceString } from "../../utils/freqFuncs";
 
 function Component({ id, data, isConnectable }: NodeProps<ComponentType>) {
+  const typeOfComponent =
+    data.reply_markup_supported ?
+      "group/component text-top relative flex h-24 w-44 cursor-pointer justify-center rounded-lg border-2 border-base-content bg-primary px-1 text-primary-content shadow-lg hover:bg-base-100 hover:text-base-content"
+    : "group/component relative flex h-12 w-24 cursor-pointer items-center justify-center rounded-lg border-2 border-base-content bg-primary px-1 text-center text-primary-content shadow-lg hover:bg-base-100 hover:text-base-content";
+
   return (
     <div className="flex flex-col">
       <div className="group flex flex-col items-center gap-1">
         <ComponentHeader id={id} data={data} />
-        <div className="group/component relative flex h-12 w-24 cursor-pointer items-center justify-center rounded-lg border-2 border-base-content bg-primary px-1 text-center text-primary-content shadow-lg hover:bg-base-100 hover:text-base-content">
+        <div className={typeOfComponent}>
           <ComponentHandles component={data} isConnectable={isConnectable} />
           <div className="text-[10px] font-medium group-hover/component:hidden">
             {data.component_name}
