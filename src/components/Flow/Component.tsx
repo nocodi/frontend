f2f1b383af88239dfd24c920a1f5203908a385a2
arminfatahi <arminfatahi2002@gmail.com>
2025-06-12 16:03:32 +0330

    move delete function to componentOptions

--- src/components/Flow/Component.tsx
+++ src/components/Flow/Component.tsx
@@ -1,40 +1,13 @@
-import { NodeProps, useReactFlow } from "reactflow";
+import { NodeProps } from "reactflow";
 import { ComponentType } from "../../types/Component";
-import api from "../../services/api";
-import { getPathOfContent, sliceString } from "../../utils/freqFuncs";
-import { toast } from "react-toastify";
-import { useContentTypes } from "../../services/getQueries";
-import { useLoading } from "../../pages/Workflow";
 import { ComponentHeader, ComponentHandles } from "./ComponentOptions";
+import { sliceString } from "../../utils/freqFuncs";
 
 function Component({ id, data, isConnectable }: NodeProps<ComponentType>) {
-  const flowInstance = useReactFlow();
-  const setLoading = useLoading();
-  const { contentTypes } = useContentTypes();
-
-  function deleteComponent() {
-    setLoading(true);
-    if (contentTypes) {
-      api
-        .delete(
-          `${getPathOfContent(data.component_content_type, contentTypes)}${id}/`,
-        )
-        .then(() => {
-          flowInstance.deleteElements({ nodes: [{ id: id }] });
-        })
-        .catch((err) => {
-          toast(err.message);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    }
-  }
-
   return (
     <div className="flex flex-col">
       <div className="group flex flex-col items-center gap-1">
-        <ComponentHeader data={data} deleteComponent={deleteComponent} />
+        <ComponentHeader id={id} data={data} />
         <div className="group/component relative flex h-12 w-24 cursor-pointer items-center justify-center rounded-lg border-2 border-base-content bg-primary px-1 text-center text-primary-content shadow-lg hover:bg-base-100 hover:text-base-content">
           <ComponentHandles
             component_type={data.component_type}
--- src/components/Flow/ComponentOptions.tsx
+++ src/components/Flow/ComponentOptions.tsx
@@ -1,18 +1,42 @@
 import { Cog, Trash2 } from "lucide-react";
 import { ComponentType } from "../../types/Component";
 import { useUnattended } from "../Context/UnattendedComponentContext";
-import { Handle, Position } from "reactflow";
+import { Handle, Position, useReactFlow } from "reactflow";
+import { useLoading } from "../../pages/Workflow";
+import { useContentTypes } from "../../services/getQueries";
+import api from "../../services/api";
+import { getPathOfContent } from "../../utils/freqFuncs";
+import { toast } from "react-toastify";
 
 type ComponentHeaderProps = {
+  id: string;
   data: ComponentType;
-  deleteComponent: () => void;
 };
 
-export const ComponentHeader = ({
-  data,
-  deleteComponent,
-}: ComponentHeaderProps) => {
+export const ComponentHeader = ({ id, data }: ComponentHeaderProps) => {
+  const flowInstance = useReactFlow();
+  const setLoading = useLoading();
+  const { contentTypes } = useContentTypes();
   const setUnattendedComponent = useUnattended()[1];
+
+  function deleteComponent() {
+    setLoading(true);
+    if (contentTypes) {
+      api
+        .delete(
+          `${getPathOfContent(data.component_content_type, contentTypes)}${id}/`,
+        )
+        .then(() => {
+          flowInstance.deleteElements({ nodes: [{ id: id }] });
+        })
+        .catch((err) => {
+          toast(err.message);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }
 
   return (
     <div className="invisible flex h-3 flex-row justify-center gap-1 text-base-content opacity-0 transition-opacity duration-300 ease-in group-hover:visible group-hover:opacity-100">
