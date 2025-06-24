761f364a736cbcb8031c0371c479344e73fcabce
arminfatahi <arminfatahi2002@gmail.com>
2025-06-24 17:19:20 +0330

    update params types

--- src/types/ComponentDetailForm.ts
+++ src/types/ComponentDetailForm.ts
@@ -4,6 +4,6 @@ export type KeyboardType = "inline" | "reply";
 
 export type GridItem = {
   id: string;
-  next_component: string;
-  label: string;
+  next_component: number | null;
+  value: string;
 };
