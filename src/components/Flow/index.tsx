72ddd50a3926f1c670ebe1bfac32d630cefccc90
arminfatahi <arminfatahi2002@gmail.com>
2025-07-06 13:08:36 +0330

    provide btnCounter

--- src/components/Flow/index.tsx
+++ src/components/Flow/index.tsx
@@ -2,15 +2,18 @@ import { DnDProvider } from "../Context/DnDContext";
 import { ReactFlowProvider } from "reactflow";
 import Flow from "./Flow";
 import { UnattendedComponentProvider } from "../Context/UnattendedComponentContext";
+import { BtnCounterProvider } from "../Context/BtnCounterCtx";
 
 function DnDFlow() {
   return (
     <ReactFlowProvider>
-      <UnattendedComponentProvider>
-        <DnDProvider>
-          <Flow />
-        </DnDProvider>
-      </UnattendedComponentProvider>
+      <BtnCounterProvider>
+        <UnattendedComponentProvider>
+          <DnDProvider>
+            <Flow />
+          </DnDProvider>
+        </UnattendedComponentProvider>
+      </BtnCounterProvider>
     </ReactFlowProvider>
   );
 }
