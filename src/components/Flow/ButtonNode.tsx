b32d70ddeb7dbca32438fc7fa22f753e76dc6769
arminfatahi <arminfatahi2002@gmail.com>
2025-07-18 14:43:20 +0330

    use button name for hover

--- src/components/Flow/ButtonNode.tsx
+++ src/components/Flow/ButtonNode.tsx
@@ -1,11 +1,11 @@
 import { Handle, NodeProps, Position } from "reactflow";
 import { ComponentType } from "../../types/Component";
 
-function ButtonNode({ isConnectable }: NodeProps<ComponentType>) {
+function ButtonNode({ data, isConnectable }: NodeProps<ComponentType>) {
   return (
     <div className="tooltip">
       <div className="tooltip-content bg-base-content text-base-300">
-        <div className="text-xs">salam</div>
+        <div className="text-xs">{data.component_name}</div>
       </div>
       <div className="bg-parent h-1 w-1">
         <Handle
